ccd882df4807d283e84510386e196cb57e7886c0
arkainas <kevin.vikander@gmail.com>
2024-02-07 14:41:29 +0100

    final commit for server. Template to add new get and post commands.

--- Get.cs
+++ Get.cs
@@ -5,18 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using Npgsql;
+using System.Reflection.Metadata.Ecma335;
 
 
 namespace YungDev01;
 
 public class Get (HttpListenerResponse res, HttpListenerRequest req, NpgsqlDataSource db)
 {
-    
+
+    public string GetCommand()
+    {
+        var path = req.Url?.AbsolutePath;
+        var lastPath = req.Url?.AbsolutePath.Split("/").Last();
+        string result = string.Empty;
+
+        if (path != null && path == "/users")
+        {
+            //add logic
+        }
+        return result;
+    }
     public void GetMethod()
     {
         string message = @"
-                        connection successful
-                        ";
+                        Get command list: 
+                        "; //add list for commands for the player to use
         byte[] buffer = Encoding.UTF8.GetBytes(message);
         res.ContentType = "text/plain";
         res.StatusCode = (int)HttpStatusCode.OK;
---
+++ Post.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace YungDev01;
+
+public class Post(NpgsqlDataSource db)
+{
+    // add comands here that will post something to the database
+    public void Commands(HttpListenerRequest req, HttpListenerResponse res)
+    {
+        string? path = req.Url?.AbsolutePath;
+        string? lastPath = req.Url?.AbsolutePath.Split("/").Last();
+
+        if (path != null && path.Contains("/register"))
+        {
+            //add logic
+            StreamReader reader = new(req.InputStream, req.ContentEncoding);
+            string body = reader.ReadToEnd();
+        }
+        if (path != null && path.Contains("/moveto"))
+        {
+            //add logic
+
+        }
+    }
+}
