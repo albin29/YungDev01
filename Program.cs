cb66751b0930d41ef8fc936e2c87cb7adc79bc0b
TheAceOfJ <arvid.jannhov@hotmail.com>
2024-02-07 15:09:50 +0100

    Created players, highscore and location table Added trig§ger for adding highscore

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
+using Npgsql;
+using YungDev01;
 Console.WriteLine("Hello, world!");
+
+string dbUri = "Host=localhost;Port=5455;Username=postgres;Password=postgres;Database=YungDev";
+await using var db = NpgsqlDataSource.Create(dbUri);
+
+Table table = new Table(db);
+await table.CreateTable();
