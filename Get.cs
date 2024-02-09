5fa4a53e8fd29a1f5f80b7a15e9315fc43537103
albin29 <albin.duraku00@gmail.com>
2024-02-09 18:56:04 +0100

    fix menu string

--- Get.cs
+++ Get.cs
@@ -20,41 +20,61 @@ public class Get(HttpListenerRequest req, NpgsqlDataSource db)
     private string GetMenu()
     {
         return @"
-----------------------------------------------
-Welcome to YungDev: The Game Tutorial
-----------------------------------------------
-
-Welcome, Player, to the exciting world of YungDev! 
-
-In this game, you will embark on a thrilling adventure, facing challenges, making decisions, and shaping your destiny.
-Let's get started with a quick tutorial on how to play:
-
-1. **Registering as a Player:**
-To begin your journey, you must first register as a player. Use the following command to register:
-curl -X POST localhost:3000/register -d YourName,YourPassword
-----------------------------------------------
-2. **Exploring Player Information::**
-----------------------------------------------
-Once registered, you can explore your player information, including stats and progress.
-Use the following command to view player information:
-
-curl -X GET localhost:3000/players/YourPlayerID
-----------------------------------------------
-3. **Resting and Advancing Time:::**
-----------------------------------------------
-In YungDev, time passes as you rest. Use the following command to rest and advance to the next day:
-
-curl -X POST localhost:3000/sleep -d YourPlayerID
-----------------------------------------------
-4. **Moving to Different Locations::::**
-----------------------------------------------
-Explore various locations in the game world by moving to different areas. Use the following command to move:
-
-curl -X POST localhost:3000/moveto -d YourPlayerID,LocationID
-
-----------------------------------------------
-5. **placeholder::::**
-----------------------------------------------
+     ----------------------------------------------
+     Welcome to YungDev: The Game Tutorial
+     ----------------------------------------------
+     
+     Welcome, Player, to the exciting world of YungDev! 
+     
+     In this game, you will embark on a thrilling adventure, facing challenges, 
+     making decisions, and shaping your destiny.
+     Let's get started with a quick tutorial on how to play:
+     
+     1. **Registering as a Player:**
+     To begin your journey, you must first register as a player. 
+     Use the following command to register:
+     curl -X POST localhost:3000/register -d YourName,YourPassword
+     ----------------------------------------------
+     2. **Exploring Player Information::**
+     ----------------------------------------------
+     Once registered, you can explore your player information,
+     including stats and progress.
+     Use the following command to view player information:
+     
+     curl -X GET localhost:3000/players/YourPlayerID
+     OR
+     curl -X GET localhost:3000/players/    
+     ----------------------------------------------
+     3. **Resting and Advancing Time:::**
+     ----------------------------------------------
+     In YungDev, time passes as you rest. 
+     Use the following command to rest and advance to the next day:
+     
+     curl -X POST localhost:3000/sleep -d YourPlayerID
+     ----------------------------------------------
+     4. **Studying at Different Locations::::**
+     ----------------------------------------------
+     Explore various locations in the game world by moving to different areas. 
+     Use the following command to study:
+     
+     curl -X POST localhost:3000/study -d YourPlayerID,LocationID
+     
+     ----------------------------------------------
+     5. **You can buy items to advance your skills or stamina**
+     ----------------------------------------------
+     To see the store
+     curl -X GET localhost:3000/shop
+     ----------------------------------------------
+     To buy something
+     curl -X POST localhost:3000/shop -d playerid,itemid
+     
+     ----------------------------------------------
+     7. ** You can hack other players to steal their stats**
+     ----------------------------------------------
+     If you manage to hack the legend Arvid, 
+     you will be awarded with bonus stats
+     ----------------------------------------------
+     curl -x POST localhost:3000/hack -d yourid,victimid
 ";
     }
     public string GetCommand()
