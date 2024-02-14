46ec8db4e3962fa9602a4d1e6c1d857bf2aa1c47
DanijelGb <124356150+DanijelGb@users.noreply.github.com>
2024-02-14 12:14:05 +0100

    trimmed the sleep function

--- Update.cs
+++ Update.cs
@@ -11,37 +11,49 @@ public class Update(NpgsqlDataSource db)
 {
     public void Money(int updatedMoney, int playerId)
     {
-        string qGoodSleep = @"
+        string qUpdatedMoney = @"
         update players
         set money = $1 
         where id = $2;";
 
-        using var command = db.CreateCommand(qGoodSleep);
+        using var command = db.CreateCommand(qUpdatedMoney);
         command.Parameters.AddWithValue(updatedMoney);
         command.Parameters.AddWithValue(playerId);
         command.ExecuteNonQuery();
     }
     public void Stamina(int updatedStamina, int playerId)
     {
-        string qGoodSleep = @"
+        string qUpdatedStamina = @"
         update players
         set stamina = $1 
         where id = $2;";
 
-        using var command = db.CreateCommand(qGoodSleep);
+        using var command = db.CreateCommand(qUpdatedStamina);
         command.Parameters.AddWithValue(updatedStamina);
         command.Parameters.AddWithValue(playerId);
         command.ExecuteNonQuery();
     }
     public void Skills(int updatedSkills, int playerId)
     {
-        string qGoodSleep = @"
+        string qUpdatedSkills = @"
         update players
         set skills = $1 
         where id = $2;";
 
-        using var command = db.CreateCommand(qGoodSleep);
+        using var command = db.CreateCommand(qUpdatedSkills);
         command.Parameters.AddWithValue(updatedSkills);
+        command.Parameters.AddWithValue(playerId);
+        command.ExecuteNonQuery();
+    }
+    public void Day(int updatedDay, int playerId)
+    {
+        string qUpdatedDay = @"
+        update players
+        set skills = $1 
+        where id = $2;";
+
+        using var command = db.CreateCommand(qUpdatedDay);
+        command.Parameters.AddWithValue(updatedDay);
         command.Parameters.AddWithValue(playerId);
         command.ExecuteNonQuery();
     }
